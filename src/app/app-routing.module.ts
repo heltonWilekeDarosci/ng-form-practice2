d49b6c1aec06a02a9077d1a30c19ca3c0e934a9e
helton <heltoloco@hotmail.com>
2022-09-05 19:51:07 -0300

    feat(form): create a form for address registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FormComponent } from './form/form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FormComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
